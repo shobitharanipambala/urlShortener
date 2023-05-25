5de73ca659ddd6e194576f4706ff231ddc5f1813
kaushiksaraf91 <kaushiksaraf91@gmail.com>
2023-05-25 15:23:55 +0530

    model added

---
+++ server/model/url.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import shortid from "shortid"
+
+const env=dotenv.config()
+
+const Mongo_url="mongodb+srv://kaushik:kaush148@cluster0.habyrhy.mongodb.net/urlshortner"
+mongoose.connect(Mongo_url)
+.then(()=> {
+    console.log("connected to MOngoCloud")
+}).catch(()=> {
+    console.log("error connecting to DB")
+})
+
+const urlSchema = new mongoose.Schema({
+    alias:{
+        type:String,
+        required:true,
+    },
+    url:{
+        type:String,
+        required:true,
+    }
+})
+
+ const Url=mongoose.model("Url",urlSchema)
+
+export default Url
