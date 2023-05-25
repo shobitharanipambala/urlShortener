0132a12dbdc05fb31c640615d04a170399533de8
kaushiksaraf91 <kaushiksaraf91@gmail.com>
2023-05-25 15:12:22 +0530

    mongoDb connected

--- server/index.js
+++ server/index.js
@@ -1,14 +1,84 @@
-const express = require('express');
+import express from "express";
 const app = express();
-const cors =require ('cors')
-const shortid = require("shortid")
-app.use(cors())
+import cors from "cors";
+import shortid from "shortid";
+import Url from "./model/url.js";
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("welcome to urlshortner");
+});
 
-const short = shortid.generate();
-console.log(short)
+app.get("/mappings", async (req, res) => {
+  const geturls = await Url.find();
+  res.send(geturls);
+});
+// app.get("/r/:alias", async (req, res) => {
+//   const { id } = req.params;
+//   const foundData = await Url.find(id);
 
-app.listen(3000,()=>{
-    console.log('listening on port 3000');
-})
+//   if (foundData) {
+//     res.redirect(foundData.url);
+//   } else {
+//     res.status(404).send("Alias not found");
+//   }
+// });
+app.get("/r/:alias", async (req, res) => {
+  const { alias } = req.params;
+  const foundData = await Url.findOne({ alias: alias });
+  console.log(foundData)
+  if (foundData) {
+    res.redirect(foundData.url);
+  } else {
+    res.status(404).send("Alias not found");
+  }
+});
+app.post("/map", (req, res) => {
+  try {
+    const { alias, url } = req.body;
+    console.log(req.body);
+    const data = {
+      alias,
+      url,
+    };
+    Url.create(data);
+    res.send("added succesfully");
+  } catch (err) {
+    res.send("error in post api");
+  }
+});
+// app.post('/shorten',(req,res)=> {
+
+//     const { url } = req.body;
+//     console.log(url)
+//     const urls = {};
+//              alias = shortid.generate();
+//              urls[alias]=url;
+//             console.log( urls[alias])
+//              console.log(alias)
+//            res.send(`http://localhost/3000/${alias}`)
+
+// })
+app.post("/shorten", async (req, res) => {
+  const { url } = req.body;
+
+  const urls = {};
+  const alias = shortid.generate();
+  urls[alias] = url;
+  const data = {
+    alias,
+    url,
+  };
+  await Url.create(data);
+
+  res.send(`http://localhost/3000/r/${alias}`);
+});
+
+// const short = shortid.generate();
+// console.log(short)
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
