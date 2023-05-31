c02fe07a73cd0439ccdc62aba23d74a781477e58
Shobitha Rani Pambala <shobitharanipambala@gmail.com>
2023-05-31 23:31:08 +0530

    completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,25 @@ import React, { useState } from "react";
 function ShortenURL() {
   const [url, setURL] = useState("");
   const [shortURL, setShortURL] = useState("");
-  console.log(shortURL);
+  const [error, setError] = useState("");
 
   const handleURLChange = (event) => {
     setURL(event.target.value);
+    setError(""); // Reset the error message when input changes
   };
 
   const handleShortenClick = async () => {
     try {
+      // Validate URL input
+      if (!url) {
+        setError("Please enter a URL");
+        return;
+      }
+      const urlPattern = /^(http|https):\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+      if (!urlPattern.test(url)) {
+        setError("Please enter a valid URL");
+        return;
+      }
       const response = await fetch("http://localhost:3000/shorten", {
         method: "POST",
         headers: {
@@ -44,6 +55,7 @@ function ShortenURL() {
     <div className="box">
       <h1>POCOURL</h1>
       <input type="text" value={url} onChange={handleURLChange} />
+      {error && <p className="error">{error}</p>} {/* Display error message if present */}
       <button onClick={handleShortenClick}>Shorten URL</button>
       {shortURL && (
         <p>
@@ -58,3 +70,5 @@ function ShortenURL() {
 }
 
 export default ShortenURL;
+
+
